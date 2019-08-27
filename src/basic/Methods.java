186e6acd7eb493d3dbfb0a4679636b2d692f4cb4
Asma Begum <mdislam@live.com>
2019-08-27 19:14:52 -0400

    This is my first commit

---
+++ src/basic/Methods.java
@@ -0,0 +1,9 @@
+package basic;
+
+public class Methods {
+	
+	public void ValidateHeader() {
+		System.out.println("Header links validated");
+	}
+
+}
---
+++ src/basic/Parent.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class Parent {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("I navigated to the homepage");
+		
+		
+		Methods m =new Methods();
+		m.ValidateHeader();
+	}
+
+}
